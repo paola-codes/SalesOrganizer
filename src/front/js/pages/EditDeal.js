84e1965acc53c8c43c146bf6dbfcaf833c3b4262
paola-codes <paolasc2652@gmail.com>
2022-01-16 23:04:55 +0000

    progress on more views

--- src/front/js/pages/EditDeal.js
+++ src/front/js/pages/EditDeal.js
@@ -1,24 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const EditProfile = () => {
+export const listOfDeals = () => {
   const { store, actions } = useContext(Context);
 
-  const [updatedProfile, setUpdatedProfile] = useState({
-    full_name: store.loggedUser.full_name,
-    email: store.loggedUser.email,
-    phone: store.loggedUser.phone,
+  const [updatedDeal, setUpdatedDeal] = useState({
+    full_name: deal.full_name,
+    email: deal.full_name,
+    phone: deal.full_name,
   });
 
   const handleChange = (e) =>
-    setUpdatedProfile({ ...updatedProfile, [e.target.name]: e.target.value });
+    setUpdatedDeal({ ...updatedDeal, [e.target.name]: e.target.value });
 
   return (
     <div className="container py-4 px-3 text-center text-dark fs-4 my-3">
-      <h1 className="text-center my-5">Edit Profile Information</h1>
+      <h1 className="text-center my-5">Edit Deal Information</h1>
 
-      <h2>Change your information on the fields below</h2>
+      <h2>Change deal information on the fields below</h2>
 
       <form className="text-start">
         <div className="form-group my-1">
@@ -29,7 +29,7 @@ export const EditProfile = () => {
             placeholder="Edit Full Name"
             name="full_name"
             onChange={handleChange}
-            value={updatedProfile.full_name}
+            value={updatedDeal.full_name}
           />
         </div>
         <div className="form-group my-1">
@@ -40,7 +40,7 @@ export const EditProfile = () => {
             placeholder="Edit email"
             name="email"
             onChange={handleChange}
-            value={updatedProfile.email}
+            value={updatedDeal.email}
           />
         </div>
         <div className="form-group my-1">
@@ -51,15 +51,15 @@ export const EditProfile = () => {
             placeholder="Edit Phone Number"
             name="phone"
             onChange={handleChange}
-            value={updatedProfile.phone}
+            value={updatedDeal.phone}
           />
         </div>
       </form>
-      <Link to="/Profile">
+      <Link to="/HomePage">
         <button
           className="btn btn-warning btn-lg p-2 w-75 m-auto my-3"
           onClick={() => {
-            actions.updateProfile(updatedProfile, store.loggedUser.id);
+            actions.updateDeal(updatedDeal, deal.id);
           }}
         >
           Save Changes
---
+++ src/front/js/pages/Profile.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Profile = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="container py-4 px-3 text-center text-light fs-4 mt-3">
+      <h1 className="text-center my-5">Profile</h1>
+
+      <div className="row d-flex justify-content-center mb-3">
+        <div className="col-9 mx-4 text-start bg-dark bg-opacity-75 shadow-lg p-3">
+          <h3 className="text-center">
+            <strong>Profile Information</strong>
+          </h3>
+          <h5>
+            <strong>Full Name: </strong>
+            {store.loggedUser.full_name}
+          </h5>
+          <h5>
+            <strong>Email: </strong>
+            {store.loggedUser.email}
+          </h5>
+          <h5>
+            <strong>Phone: </strong>
+            {store.loggedUser.phone}
+          </h5>
+        </div>
+      </div>
+      <div className="row my-3 d-flex flex-column justify-content-center">
+        <Link to="/EditProfile">
+          <button className="btn btn-warning btn-lg p-2 w-75 m-auto my-3">
+            Edit Profile
+          </button>
+        </Link>
+        <Link to="/HomePage">
+          <button className="btn btn-warning btn-lg p-2 w-75 m-auto my-3">
+            Back to Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
