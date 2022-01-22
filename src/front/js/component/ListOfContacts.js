457d5dd9e5e7dd58fc09fe7bba8e479f4acea0b4
paola-codes <paolasc2652@gmail.com>
2022-01-22 01:52:38 +0000

    updates

--- src/front/js/component/ListOfContacts.js
+++ src/front/js/component/ListOfContacts.js
@@ -12,49 +12,98 @@ export const ListOfContacts = () => {
   const [name, setName] = useState("");
 
   return (
-    <div className="container text-center text-dark mt-0 p-0">
-      <ul
-        className="list-group position-relative mx-auto mt-2"
-        style={{ width: "80%" }}
-      >
+    <div style={{ width: "85%" }} className="text-center text-dark m-auto p-0">
+      <ul className="list-group position-relative d-flex justify-content-center">
+        <div className="input-group mb-3 mx-auto">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Filter by Name"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+            onChange={(e) =>
+              setName(e.target.value.toLowerCase().replace(" ", ""))
+            }
+          />
+        </div>
         {store.listOfContacts.length > 0
-          ? store.listOfContacts.map((contact, index) => {
-              return (
-                <li
-                  className="list-group-item d-flex flex-column mx-1 mb-3 shadow-lg border border-primary border-4 rounded-3"
-                  key={index}
-                >
-                  <p className="mx-2 m-1 text-start">
-                    <strong>Full Name:</strong> {contact.full_name}
-                  </p>
-                  <p className="mx-2 m-1 text-start">
-                    <strong>Email:</strong> {contact.email}
-                  </p>
-                  <p className="mx-2 m-1 text-start">
-                    <strong>Phone:</strong> {contact.phone}
-                  </p>
-                  <p className="text-center m-1 mt-3">
-                    <Link to={`/ContactDetails/${contact.id}`}>
-                      <button className="btn btn-primary my-2 me-2 fs-5">
-                        Details
+          ? !name
+            ? store.listOfContacts.map((contact, index) => {
+                return (
+                  <li
+                    className="list-group-item d-flex flex-column mb-3 border shadow-lg border-primary border-4 rounded-3 mx-auto"
+                    key={index}
+                  >
+                    <p className="mx-2 m-1 text-start">
+                      <strong>Full Name:</strong> {contact.full_name}
+                    </p>
+                    <p className="mx-2 m-1 text-start">
+                      <strong>Email:</strong> {contact.email}
+                    </p>
+                    <p className="mx-2 m-1 text-start">
+                      <strong>Phone:</strong> {contact.phone}
+                    </p>
+                    <p className="text-center m-1 mt-3">
+                      <Link to={`/ContactDetails/${contact.id}`}>
+                        <button className="btn btn-primary my-2 me-2 fs-5">
+                          Details
+                        </button>
+                      </Link>
+                      <Link to={`/EditContact/${contact.id}`}>
+                        <button className="btn btn-dark my-2 me-2 fs-5">
+                          Edit
+                        </button>
+                      </Link>
+                      <button
+                        type="button"
+                        className="btn btn-danger my-2 fs-5"
+                        onClick={() => actions.deleteContact(contact.id)}
+                      >
+                        Delete
                       </button>
-                    </Link>
-                    <Link to={`/EditContact/${contact.id}`}>
-                      <button className="btn btn-dark my-2 me-2 fs-5">
-                        Edit
-                      </button>
-                    </Link>
-                    <button
-                      type="button"
-                      className="btn btn-danger my-2 fs-5"
-                      onClick={() => actions.deleteContact(contact.id)}
+                    </p>
+                  </li>
+                );
+              })
+            : store.listOfContacts
+                .filter((contact, index) => contact.full_name.includes(name))
+                .map((contact, index) => {
+                  return (
+                    <li
+                      className="list-group-item d-flex flex-column mb-3 border shadow-lg border-primary border-4 rounded-3 mx-auto"
+                      key={index}
                     >
-                      Delete
-                    </button>
-                  </p>
-                </li>
-              );
-            })
+                      <p className="mx-2 m-1 text-start">
+                        <strong>Full Name:</strong> {contact.full_name}
+                      </p>
+                      <p className="mx-2 m-1 text-start">
+                        <strong>Email:</strong> {contact.email}
+                      </p>
+                      <p className="mx-2 m-1 text-start">
+                        <strong>Phone:</strong> {contact.phone}
+                      </p>
+                      <p className="text-center m-1 mt-3">
+                        <Link to={`/ContactDetails/${contact.id}`}>
+                          <button className="btn btn-primary my-2 me-2 fs-5">
+                            Details
+                          </button>
+                        </Link>
+                        <Link to={`/EditContact/${contact.id}`}>
+                          <button className="btn btn-dark my-2 me-2 fs-5">
+                            Edit
+                          </button>
+                        </Link>
+                        <button
+                          type="button"
+                          className="btn btn-danger my-2 fs-5"
+                          onClick={() => actions.deleteContact(contact.id)}
+                        >
+                          Delete
+                        </button>
+                      </p>
+                    </li>
+                  );
+                })
           : "Loading..."}
       </ul>
     </div>
